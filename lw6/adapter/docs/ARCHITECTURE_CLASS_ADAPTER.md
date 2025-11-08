a9bb3afb6c786d11aec51261f6d2ece1ff0c8c5d
cetusw <kugelev.m@gmail.com>
2025-11-08 06:38:39 +0300

    adapter Переделал на адаптер класса

---
+++ lw6/adapter/docs/ARCHITECTURE_CLASS_ADAPTER.md
@@ -0,0 +1,31 @@
+```mermaid
+classDiagram
+    direction BT
+
+    class CanvasPainter {
+        +Draw(CanvasDrawable)
+    }
+
+    class Canvas {
+        <<Interface>>
+        +MoveTo(x, y)
+        +LineTo(x, y)
+    }
+
+    class ModernGraphicsRenderer {
+        +BeginDraw()
+        +DrawLine(start, end)
+        +EndDraw()
+    }
+
+    class ModernRendererAdapter {
+        -renderer ModernGraphicsRenderer
+        -currentPos Point
+        +MoveTo(x, y)
+        +LineTo(x, y)
+    }
+
+    CanvasPainter --> Canvas
+    ModernRendererAdapter ..|> Canvas
+    ModernRendererAdapter --|> ModernGraphicsRenderer
+```
--- lw6/adapter/pkg/adapter/modernadapter.go
+++ lw6/adapter/pkg/adapter/modernadapter.go
@@ -7,14 +7,14 @@ import (
 )
 
 type modernRendererAdapter struct {
-	renderer   *moderngraphicslib.ModernGraphicsRenderer
+	*moderngraphicslib.ModernGraphicsRenderer
 	currentPos model.Point
 }
 
 func NewModernRendererAdapter(renderer *moderngraphicslib.ModernGraphicsRenderer) graphicslib.Canvas {
 	return &modernRendererAdapter{
-		renderer:   renderer,
-		currentPos: model.Point{X: 0, Y: 0},
+		ModernGraphicsRenderer: renderer,
+		currentPos:             model.Point{X: 0, Y: 0},
 	}
 }
 
@@ -24,6 +24,6 @@ func (a *modernRendererAdapter) MoveTo(x, y int) {
 
 func (a *modernRendererAdapter) LineTo(x, y int) {
 	endPoint := model.Point{X: x, Y: y}
-	a.renderer.DrawLine(a.currentPos, endPoint)
+	a.DrawLine(a.currentPos, endPoint)
 	a.currentPos = endPoint
 }
