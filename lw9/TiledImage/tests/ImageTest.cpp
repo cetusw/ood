a2a60e71c474f5b249267b2822742ad794026d22
cetusw <kugelev.m@gmail.com>
2025-12-13 06:16:16 +0300

    TiledImage поправил тесты

--- lw9/TiledImage/tests/ImageTest.cpp
+++ lw9/TiledImage/tests/ImageTest.cpp
@@ -1,45 +1,48 @@
-#include <gtest/gtest.h>
 #include "image.h"
 #include "tile.h"
+#include <gtest/gtest.h>
 
-class ImageTest : public ::testing::Test {
+class ImageTest : public ::testing::Test
+{
 };
 
-TEST_F(ImageTest, ConstructorValid) {
-	Size sz{10, 20};
-	Color bg = ToColor(10, 20, 30);
-	Image img(sz, bg);
+TEST_F(ImageTest, ConstructorValid)
+{
+	constexpr Size sz{ 10, 20 };
+	constexpr Color bg = ToColor(10, 20, 30);
+	const Image img(sz, bg);
 
-	EXPECT_EQ(img.GetPixel({0, 0}), bg);
-	EXPECT_EQ(img.GetPixel({9, 19}), bg);
+	EXPECT_EQ(img.GetPixel({ 0, 0 }), bg);
+	EXPECT_EQ(img.GetPixel({ 9, 19 }), bg);
 }
 
-TEST_F(ImageTest, OutOfBoundsAccess) {
-	Image img({10, 10}, ToColor(255, 255, 255));
-	// Out of bounds returns 0 (black)
-	EXPECT_EQ(img.GetPixel({-1, 5}), 0);
+TEST_F(ImageTest, OutOfBoundsAccess)
+{
+	Image img({ 10, 10 }, ToColor(255, 255, 255));
+	EXPECT_EQ(img.GetPixel({ -1, 5 }), 0);
 
-	img.SetPixel({100, 100}, ToColor(1, 1, 1));
+	img.SetPixel({ 100, 100 }, ToColor(1, 1, 1));
 	EXPECT_EQ(img.GetSize().width, 10);
 }
 
-TEST_F(ImageTest, CoW_ModifyTriggersCopy) {
-	int baseline = Tile::GetInstanceCount();
+TEST_F(ImageTest, CoWModifyTriggersCopy)
+{
+	const int baseline = Tile::GetInstanceCount();
 
-	Image img({16, 16}, 0); // 2x2 tiles
+	Image img({ 16, 16 }, 0);
 	EXPECT_EQ(Tile::GetInstanceCount(), baseline + 1);
 
-	img.SetPixel({0, 0}, ToColor(128, 128, 128));
-	// Should trigger copy of first tile
+	img.SetPixel({ 0, 0 }, ToColor(128, 128, 128));
 	EXPECT_EQ(Tile::GetInstanceCount(), baseline + 2);
-	EXPECT_EQ(img.GetPixel({0, 0}), ToColor(128, 128, 128));
+	EXPECT_EQ(img.GetPixel({ 0, 0 }), ToColor(128, 128, 128));
 }
 
-TEST_F(ImageTest, CoW_ReadDoesNotCopy) {
-	int baseline = Tile::GetInstanceCount();
-	Image img({16, 16}, 0);
+TEST_F(ImageTest, CoWReadDoesNotCopy)
+{
+	const int baseline = Tile::GetInstanceCount();
+	const Image img({ 16, 16 }, 0);
 
-	Color c = img.GetPixel({5, 5});
+	const Color c = img.GetPixel({ 5, 5 });
 	EXPECT_EQ(c, 0);
 	EXPECT_EQ(Tile::GetInstanceCount(), baseline + 1);
 }
--- lw9/TiledImage/tests/TileTest.cpp
+++ lw9/TiledImage/tests/TileTest.cpp
@@ -1,61 +1,69 @@
-#include <gtest/gtest.h>
 #include "tile.h"
+#include <gtest/gtest.h>
 
-class TileTest : public ::testing::Test {
+class TileTest : public ::testing::Test
+{
 };
 
-TEST_F(TileTest, ConstructorFillsWithColor) {
-	const Color bg = ToColor(100, 100, 100);
-	Tile tile(bg);
+TEST_F(TileTest, ConstructorFillsWithColor)
+{
+	constexpr Color bg = ToColor(100, 100, 100);
+	const Tile tile(bg);
 
-	for (int y = 0; y < Tile::SIZE; ++y) {
-		for (int x = 0; x < Tile::SIZE; ++x) {
-			EXPECT_EQ(tile.GetPixel({x, y}), bg);
+	for (int y = 0; y < Tile::SIZE; ++y)
+	{
+		for (int x = 0; x < Tile::SIZE; ++x)
+		{
+			EXPECT_EQ(tile.GetPixel({ x, y }), bg);
 		}
 	}
 }
 
-TEST_F(TileTest, SetAndGetPixelValid) {
+TEST_F(TileTest, SetAndGetPixelValid)
+{
 	Tile tile(0);
-	Point p = {4, 4};
-	Color color = ToColor(255, 0, 0);
+	constexpr Point p = { 4, 4 };
+	constexpr Color color = ToColor(255, 0, 0);
 
 	tile.SetPixel(p, color);
 	EXPECT_EQ(tile.GetPixel(p), color);
-	EXPECT_EQ(tile.GetPixel({3, 4}), 0);
+	EXPECT_EQ(tile.GetPixel({ 3, 4 }), 0);
 }
 
-TEST_F(TileTest, OutOfBoundsAccess) {
+TEST_F(TileTest, OutOfBoundsAccess)
+{
 	Tile tile(ToColor(255, 255, 255));
-	std::vector<Point> outPoints = {
-		{-1, 0}, {0, -1},
-		{Tile::SIZE, 0}, {0, Tile::SIZE}
+	const std::vector<Point> outPoints = {
+		{ -1, 0 }, { 0, -1 },
+		{ Tile::SIZE, 0 }, { 0, Tile::SIZE }
 	};
 
-	for (const auto& p : outPoints) {
+	for (const auto& p : outPoints)
+	{
 		tile.SetPixel(p, ToColor(0, 0, 0));
-		// GetPixel returns 0 (black) out of bounds logic in Tile::GetPixel
 		EXPECT_EQ(tile.GetPixel(p), 0);
 	}
 }
 
-TEST_F(TileTest, CopyConstructorDeepCopy) {
-	Color c1 = ToColor(1, 1, 1);
-	Color c2 = ToColor(2, 2, 2);
+TEST_F(TileTest, CopyConstructorDeepCopy)
+{
+	constexpr Color c1 = ToColor(1, 1, 1);
+	constexpr Color c2 = ToColor(2, 2, 2);
 
 	Tile t1(c1);
-	t1.SetPixel({0, 0}, c2);
+	t1.SetPixel({ 0, 0 }, c2);
 
 	Tile t2 = t1;
-	EXPECT_EQ(t2.GetPixel({0, 0}), c2);
+	EXPECT_EQ(t2.GetPixel({ 0, 0 }), c2);
 
-	t2.SetPixel({0, 0}, c1);
-	EXPECT_EQ(t2.GetPixel({0, 0}), c1);
-	EXPECT_EQ(t1.GetPixel({0, 0}), c2);
+	t2.SetPixel({ 0, 0 }, c1);
+	EXPECT_EQ(t2.GetPixel({ 0, 0 }), c1);
+	EXPECT_EQ(t1.GetPixel({ 0, 0 }), c2);
 }
 
-TEST_F(TileTest, InstanceCounting) {
-	int initialCount = Tile::GetInstanceCount();
+TEST_F(TileTest, InstanceCounting)
+{
+	const int initialCount = Tile::GetInstanceCount();
 	{
 		Tile t1(0);
 		EXPECT_EQ(Tile::GetInstanceCount(), initialCount + 1);
